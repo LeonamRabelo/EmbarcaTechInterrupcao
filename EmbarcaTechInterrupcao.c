2469f94540e82b2368c2eef96b787aded6c25911
Leonam Rabelo <rabeloleonam@gmail.com>
2025-01-28 06:11:56 -0300

    Correção na GPIO do led vermelho e testes

--- EmbarcaTechInterrupcao.c
+++ EmbarcaTechInterrupcao.c
@@ -8,7 +8,7 @@
 #define IS_RGBW false
 #define NUM_PIXELS 25
 #define WS2812_PIN 7
-#define LED_PIN_RED 11
+#define LED_PIN_RED 13
 
 // Variável global para armazenar a cor (Entre 0 e 255 para intensidade)
 uint8_t led_r = 0; // Intensidade do vermelho
@@ -23,26 +23,6 @@ static inline void put_pixel(uint32_t pixel_grb)
 static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b)
 {
     return ((uint32_t)(r) << 8) | ((uint32_t)(g) << 16) | (uint32_t)(b);
-}
-
-
-void set_one_led(uint8_t r, uint8_t g, uint8_t b)
-{
-    // Define a cor com base nos parâmetros fornecidos
-    uint32_t color = urgb_u32(r, g, b);
-
-    // Define todos os LEDs com a cor especificada
-    for (int i = 0; i < NUM_PIXELS; i++)
-    {
-        if (led_Nove[i])
-        {
-            put_pixel(color); // Liga o LED com um no buffer
-        }
-        else
-        {
-            put_pixel(0);  // Desliga os LEDs com zero no buffer
-        }
-    }
 }
 
 // Número 0
@@ -135,6 +115,25 @@ bool led_Nove[NUM_PIXELS] = {
     0, 1, 1, 1, 0   
 };
 
+void set_one_led(uint8_t r, uint8_t g, uint8_t b)
+{
+    // Define a cor com base nos parâmetros fornecidos
+    uint32_t color = urgb_u32(r, g, b);
+
+    // Define todos os LEDs com a cor especificada
+    for (int i = 0; i < NUM_PIXELS; i++)
+    {
+        if (led_Nove[i])
+        {
+            put_pixel(color); // Liga o LED com um no buffer
+        }
+        else
+        {
+            put_pixel(0);  // Desliga os LEDs com zero no buffer
+        }
+    }
+}
+
 void InicializarGPIOs(){
     stdio_init_all();
     gpio_init(LED_PIN_RED);
@@ -143,12 +142,9 @@ void InicializarGPIOs(){
 }
 
 void BlinkLedRed(){
-   while(1){
     gpio_put(LED_PIN_RED, 1);
     sleep_ms(100); 
     gpio_put(LED_PIN_RED, 0); 
-    sleep_ms(100);
-   } 
 }
 
 int main(){
@@ -157,9 +153,12 @@ int main(){
     uint offset = pio_add_program(pio, &ws2812_program);
     ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
 
+    InicializarGPIOs();
+
     while (1)
     {
-        set_one_led(led_r, led_g, led_b);
+    BlinkLedRed();
+    set_one_led(led_r, led_g, led_b);
         //sleep_ms(tempo);
         //set_one_led(0, 0, 0);
         //sleep_ms(tempo);
